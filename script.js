4829c00db3cdb8d464e94bebc74abf3cf308a2d0
Portia Greene <pd12300@yahoo.com>
2021-10-11 19:27:34 -0400

    loops exercise

---
+++ script.js
@@ -0,0 +1,24 @@
+// Loops Exercise
+
+// 1
+for (i = 1; i < 8; i++){
+    console.log(i);
+}
+// 2
+for (i = 5; i<=25; i+=4){
+    console.log(i);
+}
+// 3a
+const wizards = ["Harry Potter", "Hermione Granger", "Ron Weasley"]
+// 3b
+for (mainCharacters of wizards){
+    console.log(mainCharacters);
+}
+// 4a
+let harryPotterMovies = 0
+// 4b
+while (harryPotterMovies < 8){
+    harryPotterMovies++;
+}
+// 4c
+console.log(harryPotterMovies);
